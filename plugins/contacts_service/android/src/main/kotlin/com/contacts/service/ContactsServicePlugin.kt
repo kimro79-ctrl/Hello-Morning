2f2305ead8ae91bdb5ed78c4c781415164b158e9
kimro79-ctrl <kimro79@gmail.com>
2026-04-30 13:38:28 +0900

    Make channel nullable and update method call handling

--- plugins/contacts_service/android/src/main/kotlin/com/contacts/service/ContactsServicePlugin.kt
+++ plugins/contacts_service/android/src/main/kotlin/com/contacts/service/ContactsServicePlugin.kt
@@ -4,14 +4,14 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodChannel
 
 class ContactsServicePlugin: FlutterPlugin {
-  private lateinit var channel : MethodChannel
+  private var channel : MethodChannel? = null // ✅ ? 추가
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "github.com/baseflow/contacts_service")
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+    channel?.setMethodCallHandler(null) // ✅ ?. 사용
+    channel = null
   }
 }
-
