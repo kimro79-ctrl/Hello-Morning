faa9068dd4d87c938d40ca6feecbbfee18b62660
kimro79-ctrl <kimro79@gmail.com>
2026-04-30 13:30:43 +0900

    Implement BackgroundSmsPlugin for Flutter

---
+++ plugins/background_sms/android/src/main/kotlin/com/background/sms/BackgroundSmsPlugin.kt
@@ -0,0 +1,28 @@
+package com.background.sms
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+
+class BackgroundSmsPlugin: FlutterPlugin, MethodCallHandler {
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "background_sms")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+}
